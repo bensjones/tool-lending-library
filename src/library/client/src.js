69166d94ed6395fe6a470514ce73328e6c5f20bf
Seanna Musgrave <evilseanbot@gmail.com>
2015-10-10 22:04:51 -0700

    wish-list: Made wishlisttools a collection

--- src/library/client/src.js
+++ src/library/client/src.js
@@ -1,8 +1,13 @@
+WishlistTools = new Mongo.Collection("wishlisttool");
+
 if (Meteor.isClient) {
 	Template.wishList.helpers({
-        tools: [
+        tools: function() {
+        	return WishlistTools.find({});
+        }
+        /*[
             {name: "HTC Vive", upvotes: 5, price: 500, imageUrl: "http://www.androidcentral.com/sites/androidcentral.com/files/article_images/2015/03/htc-vive_0.jpg"},
             {name: "Perception Neuron 32 Neuron Kit", upvotes: 3, price: 1500, imageUrl: " http://cdn.crowdfundinsider.com/wp-content/uploads/2014/08/Perception-Neuron-2.jpg"}
-        ]
+        ]*/
 	});
 };
